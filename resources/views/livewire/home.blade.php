15ffd56f338cddec60058088dde1a61096d8382b
Jack Weaver <jackweaver@Jacks-Air.fibertel.com.ar>
2024-06-28 14:31:05 -0300

    wIP

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -303,7 +303,7 @@
                         <div class="flex mt-4">
                             <span class="inline-block p-2 text-vinoflow-600  md:mx-4 :text-white :bg-vinoflow-600">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    stroke-width="1.5" stroke="green" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
                                 </svg>
@@ -336,19 +336,7 @@
                                 Protección hidrófuga y anticorrosiva efectiva.
                             </div>
                         </div>
-                        <div class="flex">
-                            <span class="inline-block p-2 text-vinoflow-600  md:mx-4 :text-white :bg-vinoflow-600">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="green" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
-                                </svg>
 
-                            </span>
-                            <div class="mt-2">
-                                Buen comportamiento ante climas adversos.
-                            </div>
-                        </div>
                     </div>
                     <a href="#" wire:navigate.hover
                         class="group mt-4 flex items-center rounded-lg justify-center border border-green-600 px-5 py-2.5 text-base font-medium text-green-600 duration-150 ease-in-out hover:bg-green-800 hover:text-white sm:mt-12">
